8fb6ad6242511ca5b1e93cf54ac799d6c47e4f11
Neeraj Gupta <neeraj@finnirv.com>
2019-04-26 12:14:40 +0530

    Initial Commit. Till Chapter 80 Udemy course of Docker and Kubernetes by Stephen Grider:

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hello there Yo!
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Happy learning.
         </a>
       </header>
     </div>
